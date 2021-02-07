3f84567429bf62f257726f5aa9530f747d262e5a
Paulohenriyy <78338592+Paulohenriyy@users.noreply.github.com>
2021-02-06 21:45:05 -0300

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-  this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
+  this.sendContact(m.chat, '557191467119', 'Paulohenriyy', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
